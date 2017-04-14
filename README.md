575b676082ab629029768027eb6bb93decf62822
Davis Mosenkovs <davikovs@gmail.com>
2017-04-14 16:56:48 +0300

    Version 1.0.0
    Initial version

---
+++ README.md
@@ -0,0 +1,49 @@
+Telemetry Kill Script
+===============
+A tool for disabling Windows Telemetry (at least part of it) on Windows 
+8, 8.1 and 10. 
+
+Version 1.0.0
+
+Copyright (c) 2017 Davis Mosenkovs
+
+## Introduction
+
+There already are several instructions available for disabling Telemetry 
+on Windows. 
+However this is probably the first easy to use script for this purpose. 
+
+Use of this script should be simple enough for average users and this script 
+can be easily verified by IT professionals (to understand how it works and 
+ensure there is nothing malicious in it).
+
+## Usage
+
+The script can be downloaded (after reading and accepting `LICENSE`) from 
+this GitHub repository (e.g. [Releases](https://github.com/DavisNT/Telemetry-Kill-Script/releases) 
+section). After downloading (and extracting) file `Telemetry-Kill-Script.cmd` it 
+must be run with Administrator rights (by right clicking on it and selecting 
+_Run as Administrator_).
+
+Most likely this script will have to be used again each time when Windows Update 
+updates Telemetry related files and/or service.
+
+## Notices
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ Telemetry-Kill-Script.cmd
@@ -0,0 +1,92 @@
+@echo off
+rem Display info and license
+color f0
+echo Telemetry Kill Script 1.0.0
+echo A tool for disabling Windows Telemetry (at least part of it).
+echo https://github.com/DavisNT/Telemetry-Kill-Script
+echo.
+echo Copyright (c) 2017 Davis Mosenkovs
+echo.
+echo Permission is hereby granted, free of charge, to any person obtaining a copy
+echo of this software and associated documentation files (the "Software"), to deal
+echo in the Software without restriction, including without limitation the rights
+echo to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+echo copies of the Software, and to permit persons to whom the Software is
+echo furnished to do so, subject to the following conditions:
+echo.
+echo The above copyright notice and this permission notice shall be included in all
+echo copies or substantial portions of the Software.
+echo.
+echo THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+echo IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+echo FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+echo AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+echo LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+echo OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+echo SOFTWARE.
+echo.
+verify other 2>nul
+setlocal enableextensions
+if errorlevel 1 goto :end
+set accepted=
+if not "%1"=="/AUTO" if not "%1"=="/auto" set /P accepted=Press enter if you agree or immediately close this window otherwise! 
+if not "%accepted%"=="" goto :end
+echo.
+
+rem Check admin rights
+net session >nul 2>nul
+if errorlevel 1 goto :noadmin
+
+rem Disable Telemetry
+set errors=0
+echo Taking ownership of Telemetry files...
+takeown /A /F %SystemRoot%\system32\CompatTelRunner.exe
+if errorlevel 1 set errors=1
+takeown /A /F %SystemRoot%\system32\GeneralTel.dll
+if errorlevel 1 set errors=1
+echo.
+echo Changing permissions of Telemetry files...
+icacls %SystemRoot%\system32\CompatTelRunner.exe /deny *S-1-1-0:(X)
+if errorlevel 1 set errors=1
+icacls %SystemRoot%\system32\GeneralTel.dll /deny *S-1-1-0:(X)
+if errorlevel 1 set errors=1
+echo.
+echo Disabling Telemetry service...
+sc config DiagTrack start= disabled
+if errorlevel 1 set errors=1
+echo.
+echo Terminating Telemetry processes...
+taskkill /f /im CompatTelRunner*
+taskkill /f /im rundll32* /fi "MODULES eq GeneralTel*"
+echo.
+echo Stopping Telemetry service...
+net stop DiagTrack 2>nul
+echo.
+
+rem Display results
+if "%errors%"=="1" goto :errors
+
+rem Success
+color f2
+echo Telemetry should be disabled now.
+if not "%1"=="/AUTO" if not "%1"=="/auto" pause
+
+goto :end
+
+rem Errors occured
+:errors
+color fc
+echo Error(s) occurred, please review the script output!
+if not "%1"=="/AUTO" if not "%1"=="/auto" pause
+
+goto :end
+
+rem Missing admin rights error
+:noadmin
+color fc
+echo This script must be run with Administrator rights.
+echo Please right click on the script and select "Run as Administrator"
+if not "%1"=="/AUTO" if not "%1"=="/auto" pause
+
+rem For compatibility
+:end
